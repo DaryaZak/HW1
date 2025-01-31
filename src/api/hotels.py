b19d7ab00a7d718cd72ed107326dabc10634706c
Daria Zakomornaya <oxushime@gmail.com>
2025-01-31 14:27:40 +0300

    repos

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -1,11 +1,12 @@
 
 
 from fastapi import Query, Body, HTTPException, APIRouter
-from sqlalchemy import insert, select
+from sqlalchemy import insert, select, func
 
 from src.api.dependencies import PaginationDep
 from src.database import async_session_maker
 from src.models.hotels import HotelsOrm
+from src.repositories.hotels import HotelsRepository
 from src.shemas.hotels import Hotel, HotelPatch
 
 router = APIRouter(prefix='/hotels', tags=['Отели'])
@@ -15,29 +16,20 @@ router = APIRouter(prefix='/hotels', tags=['Отели'])
 @router.get('')
 async def get_hotels(
         pagination: PaginationDep,
-        id: int | None = Query(None,description='Айдишник'),
+        location: str | None = Query(None,description='локация'),
         title: str | None = Query(None,description='Название отеля'),
 
 ):
     per_page = pagination.per_page or 5
-
     async with async_session_maker() as session:
-        query = select(HotelsOrm)
-        if id:
-            query = query.filter_by(id=id)
-        if title:
-            query = query.filter_by(title=title)
-        query = (
-            query
-            .limit(per_page)
-            .offset(per_page * (pagination.page - 1))
+        return await HotelsRepository(session).get_all(
+            location=location,
+            title=title,
+            limit=per_page,
+            offset=per_page * (pagination.page - 1)
         )
 
 
-
-        result = await session.execute(query)
-        hotels = result.scalars().all()
-        return hotels
 
 
     #if pagination.page and pagination.per_page:
