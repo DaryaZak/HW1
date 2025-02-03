abc836032b1fdd1187f52dfc89519b836eced77b
Daria Zakomornaya <oxushime@gmail.com>
2025-02-03 10:12:41 +0300

    rep

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -1,11 +1,8 @@
 
 
-from fastapi import Query, Body, HTTPException, APIRouter
-from sqlalchemy import insert, select, func
-
+from fastapi import Query, Body, APIRouter
 from src.api.dependencies import PaginationDep
 from src.database import async_session_maker
-from src.models.hotels import HotelsOrm
 from src.repositories.hotels import HotelsRepository
 from src.shemas.hotels import Hotel, HotelPatch
 
@@ -30,11 +27,10 @@ async def get_hotels(
         )
 
 
-
-
-    #if pagination.page and pagination.per_page:
-        #return hotels_[pagination.per_page * (pagination.page-1):][:pagination.per_page]
-
+# @router.get('')
+# async def get_hotel(hotel_id:int):
+#     async with async_session_maker() as session:
+#         return await HotelsRepository(session).get_id(id=id)
 
 
 
@@ -67,42 +63,24 @@ async def create_hotel(hotel_data: Hotel = Body(openapi_examples={
     return {'status': 'OK', 'data': hotel}
 
 @router.delete('/{hotel_id}')
-def delete_hotel(hotel_id: int):
-    global hotels
-    hotels = [hotel for hotel in hotels if hotel['id'] != hotel_id]
+async def delete_hotel(hotel_id: int):
+    async with async_session_maker() as session:
+        await HotelsRepository(session).delete(id=hotel_id)
+        await session.commit()
     return {'status': 'OK'}
 
 @router.put('/{hotel_id}')
-def update_hotel(hotel_id: int, hotel_data: Hotel, update_data: dict = Body(...)):
-    if len(update_data) == 1:
-        raise HTTPException(
-            status_code=400,
-            detail="Both 'title' and 'name' must be provided together"
-        )
-
-    if len(update_data) == 0:
-        raise HTTPException(
-            status_code=400,
-            detail="Request body cannot be empty"
-        )
-
-    for hotel in hotels:
-        if hotel['id'] == hotel_id:
-            hotel.update(update_data)
-            return {"message": "Hotel updated", "hotel": hotel}
-
-    raise HTTPException(status_code=404, detail="Hotel not found")
+async def update_hotel(hotel_id: int, hotel_data: Hotel):
+    async with async_session_maker() as session:
+        await HotelsRepository(session).edit(hotel_data, id=hotel_id)
+        await session.commit()
+    return {'status': 'OK'}
 
 
 @router.patch('/{hotel_id}')
-def update_hotel_part(hotel_id: int, hotel_data: HotelPatch, update_data: dict = Body(...)):
-    for hotel in hotels:
-        if hotel['id'] == hotel_id:
-            if 'title' in update_data:
-                hotel['title'] = update_data['title']
-            if 'name' in update_data:
-                hotel['name'] = update_data['name']
-            return {"message": "Hotel updated", "hotel": hotel}
-
-    raise HTTPException(status_code=404, detail="Hotel not found")
+async def update_hotel_part(hotel_id: int, hotel_data: HotelPatch):
+    async with async_session_maker() as session:
+        await HotelsRepository(session).edit(hotel_data,exclude_unset = True, id=hotel_id)
+        await session.commit()
+    return {'status': 'OK'}
 
--- src/repositories/base.py
+++ src/repositories/base.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from sqlalchemy import select, insert
+from sqlalchemy import select, insert, update, delete
 
 
 
@@ -30,10 +30,12 @@ class BaseRepository:
 
 
 
-    # async def edit(self, data: BaseModel, **filter_by) -> None:
-    #     ...
-    #
-    #
-    #
-    # async def delete(self,**filter_by) -> None:
-    #     ...
+    async def edit(self, data: BaseModel, exclude_unset: bool = False, **filter_by) -> None:
+        update_stmt = update(self.model).filter_by(**filter_by).values(**data.model_dump(exclude_unset=exclude_unset))
+        await self.session.execute(update_stmt)
+
+
+
+    async def delete(self,**filter_by) -> None:
+        delete_stmt = delete(self.model).filter_by(**filter_by)
+        await self.session.execute(delete_stmt)
