ce6bdb9ec9e6052fedbf339d6f2a8294b7edbed1
Daria Zakomornaya <oxushime@gmail.com>
2025-02-05 10:10:39 +0300

    token

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,8 +1,11 @@
-from fastapi import APIRouter
+from os import access
 
-from passlib.context import CryptContext
+from fastapi import APIRouter, HTTPException, Response, Request
+
+
 from src.database import async_session_maker
 from src.repositories.users import UsersRepository
+from src.services.auth import AuthService
 
 from src.shemas.users import UserRequestAdd, UserAdd
 
@@ -12,16 +15,39 @@ router = APIRouter(prefix="/auth",tags=["Авторизация и аутент�
 
 
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
 @router.post("/register")
 async def register_user(
         data: UserRequestAdd,
+
 ):
-    hashed_password = pwd_context.hash(data.password)
+    hashed_password = AuthService().hash_password(data.password)
     new_user_data  = UserAdd(email=data.email, hashed_password=hashed_password)
     async with async_session_maker() as session:
         await UsersRepository(session).add(new_user_data)
         await session.commit()
 
     return {'status': 'OK'}
+
+
+@router.post("/login")
+async def login_user(
+        data: UserRequestAdd,
+        response: Response
+):
+    async with async_session_maker() as session:
+        user = await UsersRepository(session).get_user_with_hashed_password(email=data.email)
+        if not user:
+            raise HTTPException(status_code=401, detail="Пользователь с таким email не существует")
+        if not AuthService().verify_password(data.password, user.hashed_password):
+            raise HTTPException(status_code=401, detail="Пароль неверный")
+        access_token = AuthService().create_access_token({"user_id": user.id})
+        response.set_cookie("access_token", access_token)
+        return {'access_token': access_token}
+
+
+
+@router.get("/only_auth")
+async def only_auth(
+        request: Request
+):
+    access_token = ""or None
