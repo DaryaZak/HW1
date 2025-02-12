23cbed02cb5b1b99687dec19fee10f59bd21ffe5
Daria Zakomornaya <oxushime@gmail.com>
2025-02-12 14:47:25 +0300

    facilities

--- src/main.py
+++ src/main.py
@@ -14,13 +14,17 @@ from src.api.hotels import router as router_hotels
 from src.api.rooms import router as router_rooms
 from src.api.auth import router as router_auth
 from src.api.bookings import router as router_bookings
+from src.api.facilities import router as router_facilities
+
 
 app = FastAPI()
 
 app.include_router(router_auth)
 app.include_router(router_hotels)
 app.include_router(router_rooms)
+app.include_router(router_facilities)
 app.include_router(router_bookings)
+
 
 #@app.get(path:'/docs', include_in_schema=False)
 #async def
---
+++ src/repositories/facilities.py
@@ -0,0 +1,15 @@
+
+
+from src.models.facilities import FacilitiesOrm, RoomsFacilitiesOrm
+from src.repositories.base import BaseRepository
+
+from src.shemas.facilities import Facility, RoomFacility
+
+
+class FacilitiesRepository(BaseRepository):
+    model = FacilitiesOrm
+    schema = Facility
+
+class RoomsFacilitiesRepository(BaseRepository):
+    model = RoomsFacilitiesOrm
+    schema = RoomFacility
