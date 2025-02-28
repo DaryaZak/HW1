5b6606ef3e38a4891aa49556ba3b3f05102b57c4
Daria Zakomornaya <oxushime@gmail.com>
2025-02-28 15:28:27 +0300

    docker compose

--- src/services/auth.py
+++ src/services/auth.py
@@ -1,11 +1,13 @@
 from datetime import datetime, timezone, timedelta
-from http.client import HTTPException
-
 from passlib.context import CryptContext
 import jwt
 
 from src.config import settings
+from src.exceptions import IncorrectTokenException, ObjectAlreadyExistsException, UserAlreadyExistsException, \
+    EmailNotRegisteredException, IncorrectPasswordException
 from src.services.base import BaseService
+from src.shemas.users import UserRequestAdd, UserAdd
+
 
 
 class AuthService(BaseService):
@@ -34,4 +36,25 @@ class AuthService(BaseService):
                 token, settings.JWT_SECRET_KEY, algorithms=[settings.JWT_ALGORITHM]
             )
         except jwt.exceptions.DecodeError:
-            raise HTTPException(status_code=401, detail="Неверный токен")
+            raise IncorrectTokenException
+
+    async def register_user(self, data: UserRequestAdd):
+        hashed_password = self.hash_password(data.password)
+        new_user_data = UserAdd(email=data.email, hashed_password=hashed_password)
+        try:
+            await self.db.users.add(new_user_data)
+            await self.db.commit()
+        except ObjectAlreadyExistsException as ex:
+            raise UserAlreadyExistsException from ex
+
+    async def login_user(self, data: UserRequestAdd) -> str:
+        user = await self.db.users.get_user_with_hashed_password(email=data.email)
+        if not user:
+            raise EmailNotRegisteredException
+        if not self.verify_password(data.password, user.hashed_password):
+            raise IncorrectPasswordException
+        access_token = self.create_access_token({"user_id": user.id})
+        return access_token
+
+    async def get_one_or_none_user(self, user_id: int):
+        return await self.db.users.get_one_or_none(id=user_id)
